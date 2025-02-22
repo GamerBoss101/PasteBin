fa64d9cbf3e98aafb5fc5823ff8ce9a109a97826
GamerBoss101 <adithcool.5@gmail.com>
2025-02-22 15:54:39 -0500

    Page Session View Fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+    import { page } from "$app/state";
+
     import Editor from "$lib/components/Editor.svelte";
     import BinManager from "$lib/components/BinManager.svelte";
+    
 </script>
 
 <main class="container mx-auto space-y-5 px-4 py-20">
     <img src="/BinChan.png" alt="BinChan" class="mx-auto w-64" />
     <h1 class="text-4xl text-center font-semibold">Paste Away!</h1>
     <Editor />
-    <BinManager />
+
+    {#if page.data.session}
+        <BinManager />
+    {/if}
+
 </main>
