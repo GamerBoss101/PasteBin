60efe23305af2fea979eb1b6d8844582d43ecb4b
GamerBoss101 <adithcool.5@gmail.com>
2025-02-22 16:06:40 -0500

    Auth JS MongoDB Fixes

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import { env } from "$env/dynamic/private";
 import Discord from "@auth/sveltekit/providers/discord";
-import client from "$lib/storage/db";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [Discord({
 		clientId: env.AUTH_DISCORD_ID,
 		clientSecret: env.AUTH_DISCORD_SECRET
 	})],
-	adapter: MongoDBAdapter(client),
 	trustHost: true
 });
